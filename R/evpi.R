1cb7e947bfe8272c0a6e343859c5d348e68381c6
Fernando Alarid-Escudero <alari006@umn.edu>
2018-01-09 15:28:10 -0600

    Update evpi.R
    Changed inputs. Instead of a matrix with Outcomes, I changed to having
    a matrix of effectiveness and a matrix of costs.

--- R/evpi.R
+++ R/evpi.R
@@ -2,47 +2,52 @@
 #'
 #' \code{evpi} is used to compute the expected value of perfect information 
 #' (EVPI) from a probabilistic sensitivity analysis (PSA) dataset.
-#' @param wtp Numeric vector with willingness-to-pay (WTP) thresholds
+#' @param v.wtp Numeric vector with willingness-to-pay (WTP) thresholds
+#' @param m.e Matrix of effectiveness. Each column corresponds to a vector of
+#' effectiveness.
+#' @param m.c Matrix of costs. Each column corresponds to a vector of
+#' costs.
 #' @param Outcomes Matrix with the model outputs. The outcomes must be ordered 
 #' in such a way that for each strategy the cost must appear first then the 
 #' effectiveness.
 #' @keywords expected value of perfect information; net monetary benefit
 #' @section Details:
 #' \code{evpi} calculates the value of eliminating all the uncertainty of a 
-#' cost-effectiveness analysis at each \code{wtp} threshold.
+#' cost-effectiveness analysis at each WTP threshold.
 #' @return evpi A data frame with the EVPI at each WTP threshold. 
 #'
-evpi <- function(wtp, Outcomes){
+evpi <- function(v.wtp, m.e, m.c){
   # Load required packages
-  require(reshape2)
   require(matrixStats)
+  if(!(ncol(m.e) == ncol(m.c))){
+    stop("Matrices of effectiveness and costs do not have same number of strategies.")
+  }
+  if(ncol(m.e)<2){
+    stop("You need at least two different strategies to compute EVPI.")
+  }
   # Create scalar with number of simulations
-  n.sim <- nrow(Outcomes)
+  n.sim <- nrow(m.e)
   # Create scalar with number of strategies (i.e. number of columns of 
-  # `Outcomes` divided by two)
-  n.strategies <- ncol(Outcomes)/2
+  # effectiveness matrix)
+  n.str <- ncol(m.e)
   # Data frame to store EVPI for each WTP threshold
-  evpi <- as.data.frame(array(0, dim = c(length(wtp), 2)))
+  df.evpi <- as.data.frame(array(0, dim = c(length(v.wtp), 2)))
   # Name data frame's columns
-  colnames(evpi) <- c("WTP", "EVPI")
+  colnames(df.evpi) <- c("WTP", "EVPI")
   # Assign vector with WTP thresholds to first column of `evpi`
-  evpi$WTP <- wtp
-  # Vector to index costs
-  costInd <- seq(1, 2*n.strategies, by = 2)
-  # Vector to index effectiveness
-  effInd  <- seq(2, 2*n.strategies, by = 2)
+  df.evpi$WTP <- v.wtp
   # Estimate the Loss matrix and EVPI at each WTP threshold
-  for(l in 1:length(wtp)){
+  for(l in 1:length(v.wtp)){
     # Compute NMB with vector indexing
-    nmb <-  wtp[l]*Outcomes[, effInd]-Outcomes[, costInd] 
+    nmb <-  v.wtp[l]*m.e - m.c
     ## Find the optimal strategy with current info
     d.star <- which.max(colMeans(nmb))
     ## Calculate the opportunity loss from choosing d.star for each strategy
     loss <- nmb - nmb[, d.star]
     ## Compute EVPI
-    evpi$EVPI[l] <- mean(rowMaxs(as.matrix(loss))) # needs to be a numeric matrix
+    df.evpi$EVPI[l] <- mean(rowMaxs(as.matrix(loss))) # needs to be a numeric matrix
   }
   #Return a data frame
-  class(evpi) <- "evpi"
-  return(evpi)
+  class(df.evpi) <- "evpi"
+  return(df.evpi)
 }
